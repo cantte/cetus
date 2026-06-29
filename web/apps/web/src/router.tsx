9984a8393d57ea4eb65eb43a956b8cda85b2b3f0
CarlosPavajeau <cantte098@gmail.com>
2026-06-29 12:54:49 -0500

    remove header component import

--- web/apps/web/src/router.tsx
+++ web/apps/web/src/router.tsx
@@ -1,13 +1,11 @@
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 
-import Loader from "./components/loader";
 import { routeTree } from "./routeTree.gen";
 
 export const getRouter = () => {
   const router = createTanStackRouter({
     context: {},
     defaultNotFoundComponent: () => <div>Not Found</div>,
-    defaultPendingComponent: () => <Loader />,
     defaultPreloadStaleTime: 0,
     routeTree,
     scrollRestoration: true,
